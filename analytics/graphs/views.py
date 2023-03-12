5e99546832350754775ba59e2ee8c32432b54c45
VJ6991 <vishnuveena1992@gmail.com>
2023-03-12 12:20:33 +0530

    commit-3

--- analytics/graphs/views.py
+++ analytics/graphs/views.py
@@ -25,10 +25,10 @@ def linegraph(request):
 
 
 def bargraph(request):
-    likes_by_age_range = Graph.objects.values('age').annotate(num_likes=Count('likes'))
-    age_ranges = [item['age'] for item in likes_by_age_range]
-    num_likes = [item['num_likes'] for item in likes_by_age_range]
-    return render(request, 'bargraph.html', {'age_ranges': age_ranges,'num_likes': num_likes})
+    queryset = Graph.objects.all()
+    labels = [obj.age for obj in queryset]
+    data = [obj.likes for obj in queryset]
+    return render(request, 'bargraph.html', {'labels': labels, 'data': data})
     
 
 def piechart(request):
@@ -40,62 +40,11 @@ def piechart(request):
     return render(request,'piechart.html',{'graph_data':graph_data_json})
 
 def vectormap(request):
-    context = {
-        'chart_js': True,
-        'chart_data': {
-            'labels': ['US', 'Canada', 'Mexico', 'Brazil', 'Argentina'],
-            'datasets': [{
-                'label': 'Likes',
-                'data': [50, 20, 30, 40, 10],
-                'backgroundColor': [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)'
-                ],
-                'borderColor': [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)'
-                ],
-                'borderWidth': 1
-            }]
-        },
-        'chart_options': {
-            'legend': {
-                'display': False
-            },
-            'scales': {
-                'xAxes': [{
-                    'type': 'map',
-                    'position': 'top',
-                    'gridLines': {
-                        'display': False
-                    },
-                    'ticks': {
-                        'beginAtZero': True
-                    }
-                }]
-            },
-            'plugins': {
-                'map': {
-                    'center': {'lat': 37.8, 'lng': -96},
-                    'zoom': 4,
-                    'geoJSON': 'https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson',
-                    'tooltipTemplate': '<%=label%>: <%=value%> likes'
-                }
-            }
-        }
-    }
-    return render(request, 'vectormap.html', context)
-
+    data = Graph.objects.all().values('country', 'likes')
+    return render(request, 'vectormap.html', {'data': data})
 
 def registration(request):
     if request.method == 'POST':
-        # Get form data from request
         name = request.POST['name']
         email = request.POST['email']
         mobile = request.POST['mobile']
@@ -103,20 +52,18 @@ def registration(request):
         password = request.POST['password']
         confirm_password = request.POST['confirm-password']
         
-        # Check if password and confirm password match
         if password != confirm_password:
             return render(request, 'registration.html', {'error': 'Passwords do not match'})
         
-        # Check if username already exists
+        
         if User.objects.filter(username=username).exists():
             return render(request, 'registration.html', {'error': 'Username already exists'})
         
-        # Create new user
         user = User.objects.create(username=username, email=email, password=password)
         user.first_name = name
         user.save()
         
-        return redirect('login')  # Replace 'home' with your desired URL name
+        return redirect('login') 
     else:
         return render(request, 'registration.html')
 
